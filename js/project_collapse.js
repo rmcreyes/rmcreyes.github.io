f2c0c82dd65343b3f41f3a1597183721e2c8b3ed
rmcreyes <robinreyes1998@yahoo.com>
2019-01-01 18:30:19 -0800

    added twinstick shooter to project list

--- js/project_collapse.js
+++ js/project_collapse.js
@@ -1,14 +1,13 @@
-var coll = document.getElementsByClassName("collapsible");
+var projects = document.getElementsByClassName("collapsible");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
+for (i = 0; i < projects.length; i++) {
+    projects[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
+        if (content.style.maxHeight)
             content.style.maxHeight = null;
-        } else {
+        else
             content.style.maxHeight = content.scrollHeight + "px";
-        }
     });
 }
